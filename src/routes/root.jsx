ec429e66077773b8375c980e2272358cbd1aea25
Tiffany Bacher <tiffanybacher@gmail.com>
2023-11-15 17:54:05 -0700

    Initial commit with root page and error page

---
+++ src/routes/root.jsx
@@ -0,0 +1,13 @@
+export default function Root() {
+    return (
+        <>
+            <div>
+                <h1>Unlimited movies, TV shows, and more</h1>
+                <p>Watch anywhere. Cancel anytime.</p>
+                <p>Ready to watch? Enter your email to sign in.</p>
+                <input type="text" placeholder="Email address" />
+                <button>Sign In</button>
+            </div>
+        </>
+    )
+}
---
+++ src/users.js
@@ -0,0 +1,10 @@
+export const users = {
+    1: {
+        email: 'tiffanybacher@gmail.com',
+        password: 'password1'
+    },
+    2: {
+        email: 'jeremybacher@gmail.com',
+        password: 'password2'
+    }
+}
