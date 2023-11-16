99b874d2efd609406d4e6c743a7f410585d95f8b
Tiffany Bacher <tiffanybacher@gmail.com>
2023-11-15 21:09:13 -0700

    Add header and footer and update CSS

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,14 +1,30 @@
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Root() {
     return (
         <>
-            <div>
-                <h1>Unlimited movies, TV shows, and more</h1>
-                <p>Watch anywhere. Cancel anytime.</p>
-                <p>Ready to watch? Enter your email to sign in.</p>
-                <input type="text" placeholder="Email address" />
-                <Link to="sign-in" className="button">Sign In</Link>
+            <div className="root__main">
+                <header>
+                    <div className="max-width">
+                        <h1 className="logo">WEBFLIX</h1>
+                    </div>
+                </header>
+                <div className="max-width">
+                    <h2 className="h1">Unlimited movies, TV shows, and more</h2>
+                    <p>Watch anywhere. Cancel anytime.</p>
+                    <p>Ready to watch? Enter your email to sign in.</p>
+                    <div>
+                        <input type="text" placeholder="Email address" />
+                        <Link to="sign-in" className="button">Sign In</Link>
+                    </div>
+                </div>
+                <footer>
+                    <div className="line-break"></div>
+                    <div className="footer-content max-width">
+                        <p>Questions? Call 1-844-505-2993</p>
+                        {/* TODO: Add footer links */}
+                    </div>
+                </footer>
             </div>
         </>
     )
