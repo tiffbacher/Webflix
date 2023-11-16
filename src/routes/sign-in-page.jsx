bfde9cff1fa74795ddfcf454e375ad872db8fc8d
Tiffany Bacher <tiffanybacher@gmail.com>
2023-11-15 19:33:10 -0700

    Add eye icon to sign-in password input

--- src/routes/sign-in-page.jsx
+++ src/routes/sign-in-page.jsx
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+
 export default function SignInPage() {
+    const [isPasswordInput, setIsPasswordInput] = useState(true);
+
+    const handlePasswordInputChange = () => {
+        setIsPasswordInput(!isPasswordInput);
+    }
+
     return (
         <>
             <form>
@@ -6,7 +14,10 @@ export default function SignInPage() {
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email"></input>
                 <label for="password">Password:</label>
-                <input type="password" id="password" name="password"></input>
+                <div className="password-wrapper">
+                    <input type={isPasswordInput ? 'password' : 'text'} id="password" name="password"></input>
+                    <i className={`fa-eye ${isPasswordInput ? "fa-solid" : "fa-regular"}`} id="eye" onClick={handlePasswordInputChange}></i>
+                </div>
                 <input type="submit" value="Sign In"></input>
             </form>
         </>
