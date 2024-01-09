2532c90ee19684946bcffe309760a1bac106882b
Tiffany Bacher <tiffanybacher@gmail.com>
2024-01-09 12:33:49 -0700

    Add form validation and error handling to home page

--- src/routes/root/HomePage.jsx
+++ src/routes/root/HomePage.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { validateEmail } from "@/utils/form-validators";
 
 export default function HomePage() {
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState("");
+  const [error, setError] = useState("");
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -11,7 +13,39 @@ export default function HomePage() {
 
   const validateInput = () => {
     const error = validateEmail(email);
-    if (error) console.error(error);
+    if (error) {
+      setError(error);
+      console.error(error);
+    }
+  };
+
+  const errorMessage = () => {
+    if (error) {
+      return (
+        <p className="home-page__form__error-messag text-red-600 py-2 text-xs">
+          {error}
+        </p>
+      );
+    }
+  };
+
+  const handleSubmit = () => {
+    setError("");
+    setLoading(true);
+    const error = validateInput();
+    if (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  /** TODO: Make spinner its own component */
+  const spinner = () => {
+    if (loading) {
+      return (
+        <i className="fa-solid fa-circle-notch fa-spin text-white h-4 w-4 absolute bottom-3.5 right-3" />
+      );
+    }
   };
 
   return (
@@ -21,9 +55,10 @@ export default function HomePage() {
       </h2>
       <p className="my-4">Watch anywhere. Cancel anytime.</p>
       <p className="my-4">Ready to watch? Enter your email to sign in.</p>
-      <div className="home-page__form">
+      <div className="home-page__form flex">
         {/* TODO: Create components for input and button */}
         <input
+          className={error ? "border border-solid border-red-600" : undefined}
           type="email"
           name="email"
           placeholder="Email address"
@@ -31,16 +66,25 @@ export default function HomePage() {
           onChange={(e) => handleEmailChange(e)}
         />
         {/* TODO: Turn this button into "Get Started" button that links to registration */}
-        <Link
+        {/* <Link
           to="sign-in"
           className="button ml-4 py-3 px-8"
           state={{ email: email }}
           onClick={validateInput}
         >
           Sign In
-        </Link>
-        {/* TODO: Add feedback for field validation errors */}
+        </Link> */}
+        <div className="button-wrapper relative">
+          <input
+            type="submit"
+            value="Sign In"
+            className="home-page__form__button button ml-4 px-8 bg-red-600 cursor-pointer"
+            onClick={handleSubmit}
+          />
+          {spinner()}
+        </div>
       </div>
+      {errorMessage()}
     </div>
   );
 }
