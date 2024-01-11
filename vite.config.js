5a307c642ca31fb5e1866986cda477af459aa2cf
Tiffany Bacher <tiffanybacher@gmail.com>
2024-01-10 21:37:22 -0700

    Rename base for vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Webflix/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
